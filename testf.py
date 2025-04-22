2f4943a57c6ceaae5acde48c4d34eafb0c7b79fc
=hieunguyen-lang <=hieu0356600737@gmail.com>
2025-04-22 17:31:36 +0700

    u

---
+++ testf.py
@@ -0,0 +1,25 @@
+import pika
+import json
+
+credentials = pika.PlainCredentials('admin', 'admin')
+params = pika.ConnectionParameters(
+    host='localhost',
+    port=5672,
+    virtual_host='/',
+    credentials=credentials
+)
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+
+channel.queue_declare(queue='data_crawled', durable=True)
+
+message = {"title": "Hello", "description": "Test"}
+channel.basic_publish(
+    exchange='',
+    routing_key='data_crawled',
+    body=json.dumps(message),
+    properties=pika.BasicProperties(delivery_mode=2)  # make message persistent
+)
+
+print("Sent!")
+connection.close()
